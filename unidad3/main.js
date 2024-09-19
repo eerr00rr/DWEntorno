6d83fe37219f9520e7bc97e331fde672cac3820f
eerr00rr <alex.11.monti@gmail.com>
2024-09-19 16:19:49 +0200

    sdafasdf

---
+++ unidad3/main.js
@@ -0,0 +1,48 @@
+function ejer1() {
+    let months = [
+        'Jan', 'Feb', 'Mar',
+        'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep',
+        'Oct', 'Nov', 'Dec'
+    ]
+
+    months.array.forEach(element => {
+        document.write(element + '<br>');
+    });
+}
+
+function ejer2() {
+
+}
+
+function ejer3() {
+
+}
+
+function ejer4() {
+
+}
+
+function ejer5() {
+
+}
+
+function ejer6() {
+
+}
+
+function ejer7() {
+
+}
+
+function ejer8() {
+
+}
+
+function ejer9() {
+
+}
+
+function ejer10() {
+
+}
