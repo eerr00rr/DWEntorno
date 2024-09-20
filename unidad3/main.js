c6da7ed840525224a1aa81bccd3dad549f31c8ca
eerr00rr <alex.11.monti@gmail.com>
2024-09-20 10:00:25 +0200

    asdfasdf

--- unidad3/main.js
+++ unidad3/main.js
@@ -6,17 +6,74 @@ function ejer1() {
         'Oct', 'Nov', 'Dec'
     ]
 
-    months.array.forEach(element => {
-        document.write(element + '<br>');
-    });
+    for (let i of months) {
+        document.write(i + '<br>');
+    }
 }
 
 function ejer2() {
+    var valors = [true, 5, false, "hola", "adeu", 2];
 
+    let textoMayor = '';
+    let boleanos = '';
+    let nums = [];
+    for (let item of valors) {
+        if (textoMayor < item.length) {
+            textoMayor = item;
+        }
+        if (typeof item === 'boolean') {
+            boleanos += item + ' ';
+        }
+        if (typeof item === 'number') {
+            nums.push(item);
+        }
+    }
+
+    let suma = nums[0] + nums[1];
+    let resta = nums[0] - nums[1];
+    let multi = nums[0] * nums[1];
+    let division = nums[0] / nums[1];
+    let modular = nums[0] % nums[1];
+
+    alert(
+        'El texto mayor es: ' + textoMayor + '\n' +
+        'Los boleanos son: ' + boleanos + '\n' +
+        'Suma: ' + suma + '\n' +
+        'Resta: ' + resta + '\n' +
+        'Multi: ' + multi+ '\n' +
+        'Division: ' + division + '\n' +
+        'Modular: ' + modular + '\n' 
+    );
 }
 
 function ejer3() {
+    let letras = [
+      "T", "R", "W", "A", "G", "M", "Y", 
+      "F", "P", "D", "X", "B", " N", "J", "Z", "S",
+      "Q", "V", "H", "L", "C", "K", "E"
+    ];
+    let dni = prompt('Introduzca DNI');
 
+    if (dni.length !== 9) {
+        alert('el dno tiene que tener una letra y ocho numeros');
+    }
+
+    let letra = dni.slice(-1).toUpperCase; 
+    let numeros = dni.slice(0, -1);
+
+    if (isNaN(numeros) || numeros.length != 8) {
+        alert('Falta de numeros');
+        return;
+    }
+
+    let posLetra = numeros % 23;
+    let letraCorrecta = letras[posLetra];
+
+    if (letra === letraCorrecta) {
+        alert('DNI correcta');
+    } else {
+        alert('Letra no es correcta');
+    }
 }
 
 function ejer4() {
