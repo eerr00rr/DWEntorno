b52ae9e46213f8d68dea5c7fefe19ad3d0bb54f9
eerr00rr <alex.11.monti@gmail.com>
2024-10-18 13:43:05 +0200

    asdjkfasdjkljfasjkldfaskdasdkfh

---
+++ unidad4/Bouncing Balls exemple/main.js
@@ -0,0 +1,122 @@
+// set up canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random RGB color value
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Ball {
+
+   constructor(x, y, velX, velY, color, size, timeToLive) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+      this.timeToLive = new Date().getSeconds() + timeToLive;
+   }
+
+   draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
+   }
+
+   update() {
+      if ((this.x + this.size) >= width) {
+         this.velX = -(this.velX);
+      }
+
+      if ((this.x - this.size) <= 0) {
+         this.velX = -(this.velX);
+      }
+
+      if ((this.y + this.size) >= height) {
+         this.velY = -(this.velY);
+      }
+
+      if ((this.y - this.size) <= 0) {
+         this.velY = -(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+   }
+
+   collisionDetect() {
+      for (const ball of balls) {
+         if (!(this === ball)) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+               ball.velX = -(ball.velX);
+               ball.velY = -(ball.velY);
+               this.velX = -(this.velX);
+               this.velY = -(this.velY);
+
+               const index = balls.indexOf(ball);
+               balls.splice(index, 1);
+            }
+         }
+      }
+   }
+
+}
+
+const balls = [];
+
+while (balls.length < 25) {
+   const size = random(10,20);
+   const ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size,width - size),  //position x
+      random(0 + size,height - size), //position y
+      random(-7,7),                   //velocity x
+      random(-7,7),                   //velocity y
+      randomRGB(),                    // color random
+      size,                           // size
+      random(10, 30)                  // ttl between 10 and 30 seconds
+   );
+
+  balls.push(ball);
+}
+
+function loop() {
+   ctx.fillStyle = '#000000';
+   ctx.clearRect(0, 0,  width, height);
+   ctx.fillRect(0, 0,  width, height);
+
+
+   for (const ball of balls) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+      if (ball.timeToLive === new Date().getSeconds()) {
+         const index = balls.indexOf(ball);
+         balls.splice(index, 1);
+      }
+   }
+
+   requestAnimationFrame(loop);
+}
+
+loop();
--- unidad5/EjercicioPuzle/main.js
+++ unidad5/EjercicioPuzle/main.js
@@ -1,20 +1,64 @@
-let tabla = document.createElement('table');
-
-let filas = 4;
-let columnas = 4;
-
-for (let fila = 1; fila <= filas; fila++) {
-    let tr = document.createElement('tr');
-
-    for (let columna = 1; columna <= columnas; columna++) {
-        let td = document.createElement('td');
-
-        let imagen = document.createElement('img');
-        imagen.src = columna * fila === 16 ? '' : `../ArchivosEjercicios/imagenes/${columna * fila}.gif`;
-
-        td.appendChild(imagen);
-        tr.appendChild(td);
+function mezclarImagenes() {
+    let set = new Set();
+    while (set.size < 16) {
+        let random = Math.floor(Math.random() * 16) + 1;
+        set.add(random);
     }
-    tabla.appendChild(tr);
+    return set;
 }
-document.body.appendChild(tabla);
+
+function getAdjacent(idVacio) {
+    let poisciones = {
+        'arriba': `td_${idVacio - 4}`,
+        'abajo': `td_${idVacio + 4}`,
+        'izquierda': `td_${idVacio - 1}`,
+        'derecho': `td_${idVacio + 1}`,
+    }
+
+    for (const key in poisciones) {
+        if (condition) {
+            
+        }
+    }
+}
+
+function crearTabla() {
+    let tabla = document.createElement('table');
+
+    let filas = 4;
+    let columnas = 4;
+
+    let imagenesMezcladas = Array.from(mezclarImagenes());
+    for (let fila = 0; fila < filas; fila++) {
+        let tr = document.createElement('tr');
+
+        for (let columna = 0; columna < columnas; columna++) {
+            let td = document.createElement('td');
+            let imagen = document.createElement('img');
+
+            let valor = imagenesMezcladas[fila * 4 + columna];
+            td.id = `td_${valor}`;
+            if (valor === 16) {
+                imagen.src = '../ArchivosEjercicios/imagenes/blanco.gif';
+                idVacio = td.id;
+            } else {
+                imagen.src = `../ArchivosEjercicios/imagenes/${valor}.gif`;
+            }
+            imagen.width = 100;
+
+            imagen.onclick = () => {
+                if (td.id === getAdjacent(idVacio)) {
+                    
+                }
+            };
+
+            td.appendChild(imagen);
+            tr.appendChild(td);
+        }
+        tabla.appendChild(tr);
+    }
+    document.body.appendChild(tabla);
+}
+
+let idVacio;
+crearTabla();
