f36c9a586bf7f47615be8613a54fe4bb12e82815
eerr00rr <alex.11.monti@gmail.com>
2024-11-12 09:46:17 +0100

    unfinished

--- unidad5/EjercicioPuzzle2/script.js
+++ unidad5/EjercicioPuzzle2/script.js
@@ -1,17 +1,49 @@
 const TIME_START = new Date();
+const CELLS = 16;
 let aux;
 let arrayImagenes = [];
 let posBlanco;
 
-function arrayInit() {
+function draw() {
     for (let i = 0; i < 16; i++) {
-        console.log(i);
+        let td = document.getElementById(i + 1);
+        let img = td.querySelector('img');
+
+        img.src = `imagenes/${arrayImagenes[i]}`;
+    }
+}
+
+function generarPuzzle() {
+    for (let i = 0; i < CELLS; i++) {
+        let random = Math.floor(Math.random() * 16);
+
+        aux = arrayImagenes[i];
+        arrayImagenes[i] = arrayImagenes[random];
+        arrayImagenes[random] = aux;
+    }
+}
+
+function arrayInit() {
+    for (let i = 0; i < CELLS; i++) {
         arrayImagenes[i] = `${i + 1}.gif`;
     }
     arrayImagenes[15] = "blanco.gif";
     posBlanco = 15;
 }
 
-function events()
+function test(event) {
+    console.log(event.target);
+    
+}
 
-arrayInit();
+function init() {
+    arrayInit();
+    generarPuzzle();
+    draw();
+    for (let i = 0; i < CELLS; i++) {
+        let td = document.getElementById(`${i + 1}`);
+        td.addEventListener('click', mover);
+    }
+}
+
+window.addEventListener('load', init);
