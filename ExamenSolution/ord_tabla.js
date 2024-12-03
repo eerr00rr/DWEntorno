eef1399b949756a9e56642cc150f52109fc2ad32
eerr00rr <alex.11.monti@gmail.com>
2024-12-03 12:54:35 +0100

    finished exam question

--- ExamenSolution/ord_tabla.js
+++ ExamenSolution/ord_tabla.js
@@ -5,14 +5,54 @@ window.addEventListener('load', () => {
     });
 
     this.document.querySelectorAll('img')[0].addEventListener('click', moverBajo);
-    // this.document.querySelectorAll('img')[1].addEventListener('click', moverArriba);
+    this.document.querySelectorAll('img')[1].addEventListener('click', moverArriba);
     this.document.getElementById('imagenes').className = 'oculto';
+    this.document.querySelector('body').addEventListener('click', deseleccionar);
 });
+
+function deseleccionar(e) {
+    if (e.target.nodeName !== 'LI' && e.target.nodeName !== 'IMG') {
+        let seleccionado = document.querySelector('.seleccionado');
+        seleccionado.className = '';
+
+        let imagenes = document.querySelector('#imagenes');
+        imagenes.className = 'oculto';
+    }
+}
+
+window.addEventListener('keydown', event => {
+    let tecla = event.key;
+    if (tecla === 'ArrowUp') {
+        moverArriba();
+    }
+    if (tecla === 'ArrowDown') {
+        moverBajo();
+    }
+});
+
+
+function moverArriba() {
+    let elementoSeleccionado = document.querySelector('.seleccionado');
+    let elementoArribaSeleccionado = elementoSeleccionado.nextElementSibling.nextElementSibling;
+    let elementoAbajoSeleccionado = elementoSeleccionado.previousElementSibling;
+    if (elementoAbajoSeleccionado) {
+        elementoSeleccionado.parentNode.insertBefore(elementoAbajoSeleccionado, elementoArribaSeleccionado);
+    } else {
+        elementoSeleccionado.parentNode.insertBefore(elementoSeleccionado, elementoSeleccionado.parentNode.lastElementChild);
+        elementoSeleccionado.parentNode.insertBefore(document.querySelector('#imagenes'), elementoSeleccionado.nextElementSibling);
+        moverBajo();
+    }
+}
 
 function moverBajo() {
     let elementoSeleccionado = document.querySelector('.seleccionado');
     let elementoArribaSeleccionado = elementoSeleccionado.nextElementSibling.nextElementSibling;
-    elementoSeleccionado.parentNode.insertBefore(elementoArribaSeleccionado, elementoSeleccionado);
+    if (elementoArribaSeleccionado) {
+        elementoSeleccionado.parentNode.insertBefore(elementoArribaSeleccionado, elementoSeleccionado);
+    } else {
+        elementoSeleccionado.parentNode.insertBefore(elementoSeleccionado, elementoSeleccionado.parentNode.firstChild);
+        elementoSeleccionado.parentNode.insertBefore(document.querySelector('#imagenes'), elementoSeleccionado.nextElementSibling);
+    }
 }
 
 function seleccionarElemento(event) {
@@ -32,10 +72,3 @@ function seleccionarElemento(event) {
         document.querySelector('ul').appendChild(imagenes);
     }
 }
-
-window.addEventListener('keydown', event => {
-    let tecla = event.key;
-    moverElemento(tecla);
-    console.log(event.key)
-});
-
